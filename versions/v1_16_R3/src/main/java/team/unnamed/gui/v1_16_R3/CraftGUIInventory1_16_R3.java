f2fdd4783505145c60dad1a9cc80b635b6a624ef
Neerixx <nerixmapper@hotmail.com>
2021-01-12 01:34:34 -0300

    Fixed 1.16.4 version...

--- versions/v1_16_R3/src/main/java/team/unnamed/gui/v1_16_R3/CraftGUIInventory1_16_R3.java
+++ versions/v1_16_R3/src/main/java/team/unnamed/gui/v1_16_R3/CraftGUIInventory1_16_R3.java
@@ -1,4 +1,4 @@
-package team.unnamed.gui.v1_16_R1;
+package team.unnamed.gui.v1_16_R3;
 
 import org.bukkit.craftbukkit.v1_16_R3.inventory.CraftInventoryCustom;
 import org.bukkit.inventory.Inventory;
@@ -7,11 +7,11 @@ import org.bukkit.inventory.InventoryHolder;
 import team.unnamed.gui.abstraction.menu.GUIData;
 import team.unnamed.gui.abstraction.menu.GUIInventory;
 
-public class CraftGUIInventory1_16_R1 extends CraftInventoryCustom implements GUIInventory {
+public class CraftGUIInventory1_16_R3 extends CraftInventoryCustom implements GUIInventory {
 
     private final GUIData guiData;
 
-    public CraftGUIInventory1_16_R1(InventoryHolder owner, GUIData guiData) {
+    public CraftGUIInventory1_16_R3(InventoryHolder owner, GUIData guiData) {
         super(owner, guiData.getSlots(), guiData.getTitle());
 
         this.guiData = guiData;
